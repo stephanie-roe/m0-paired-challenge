13acd1f23ca2ae279948baa55f122ce74cd09d7b
Stephanie Roe <stephanie.j.roe@gmail.com>
2022-01-12 20:16:23 -0700

    Initial Commit

---
+++ challenge/index.md
@@ -0,0 +1,6 @@
+# Book Titles
+1. The Four Agreements, Don Miguel Ruiz
+2. How to Speak, How to Listen, Mortimer J. Adler
+3. Javascript & Jquery, Jon Duckett
+4. Their Eyes Were Watching God, Zora Neale Hurston
+5. Cien Años De Soledad, Gabriel Garcia Marquez
---
+++ challenge/library.js
@@ -0,0 +1,6 @@
+var bookTitles = ["The Four Agreements" , "How to Speak, How to Listen" , "Javascript & Jquery" , "Their Eyes Were Watching God" , "Cien Años De Soledad"];
+var authors = ["Don Miguel Ruiz" , "Mortimer J. Adler" , "Jon Duckett" , "Zora Neale Hurston" , "Gabriel Garcia Marquez"];
+var numberOfPages = [300 , 400 , 150 , 570 , 280];
+var isInEnglish = [true , true , true , true , false];
+var numberOfCopies = [5 , 11 , 3 , 0 , 1];
+var isNonfiction = [true , true , true , false , false];
